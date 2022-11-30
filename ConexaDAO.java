ab6e2de15477005eb5eb04fb3a972f2b9d70e692
Jussan Silva <jussan.silva@txtecnologia.com.br>
2022-11-30 18:44:00 -0300

    conexão

--- ConexaDAO.java
+++ ConexaDAO.java
@@ -12,45 +12,58 @@ import java.util.logging.Logger;
 
 
 public class ConexaDAO {
+
+    private Connection conexao;
+    
+    public ConexaDAO() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://awsjussan.cbrcalzcoxol.us-east-1.rds.amazonaws.com:3306/aeroporto", "admin", "admin123");
+
    // private static final String URL = "jdbc:mysql://awsjussan.cbrcalzcoxol.us-east-1.rds.amazonaws.com:3306/aeroporto";
     private static final String URL = "jdbc:mysql://awsjussan.cbrcalzcoxol.us-east-1.rds.amazonaws.com/aeroporto";
     private static final String USER = "admin";
     private static final String PASS = "jussan123";
 
+    private static final String SCHEMA = "aeroporto";
+
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (SQLException ex) {
-            throw new RuntimeException("Erro na conexão: ", ex);
+            throw new RuntimeException("Erro na conexão com o Banco de dados: ", ex);
         }
     }
 
-    public static void closeConnection(Connection con) {
-        if (con != null) {
+    //Método para fechar a conexão com o banco de dados
+    public static void closeConnection(Connection conexao) {
+        if (conexao != null) {
             try {
-                con.close();
+                conexao.close();
             } catch (SQLException ex) {
                 System.err.println("Erro: " + ex);
             }
         }
     }
 
-    public static void closeConnection(Connection con, PreparedStatement stmt) {
-        closeConnection(con);
-        if (stmt != null) {
+    //Método para fechar a conexão com o banco de dados
+    public static void closeConnection(Connection conexao, PreparedStatement declara) {
+        closeConnection(conexao);
+        if (declara != null) {
             try {
-                stmt.close();
+                declara.close();
             } catch (SQLException ex) {
                 System.err.println("Erro: " + ex);
             }
         }
     }
 
-    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
-        closeConnection(con, stmt);
-        if (rs != null) {
+    //Método para fechar a conexão com o banco de dados
+    public static void closeConnection(Connection conexao, PreparedStatement declara, ResultSet retornoSet) {
+        closeConnection(conexao, declara);
+        if (retornoSet != null) {
             try {
-                rs.close();
+                retornoSet.close();
             } catch (SQLException ex) {
                 System.err.println("Erro: " + ex);
             }
